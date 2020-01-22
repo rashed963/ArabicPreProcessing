004d77b3dd4b58ff5fd0f27f2ac603999d037ed1
RoSh <rashed.sabra@gmail.com>
2020-01-22 20:45:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'ArabicPreProcessing',         # How you named your package folder (MyLib)
   packages = ['ArabicPreProcessing'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Arabic PreProcessing Functions that must applied before run any ML model.',   # Give a short description about your library
   author = 'Rashed Sabra',                   # Type in your name
   author_email = 'rashed.sabra@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/rashed963',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/rashed963/ArabicPreProcessing/archive/v_03.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/rashed963/ArabicPreProcessing/archive/v_04.tar.gz',    # I explain this later on
   keywords = ['NLP', 'pre-processing', 'Arabic'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
